1e120516d6b2a76c3bd6c622ad6d17c45adf1fa6
Bryan Luis Villafuerte <bryjho.villafuerte@yahoo.com.ph>
2021-01-30 22:46:35 +0100

    Updated link to site

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -6,7 +6,7 @@ function FooterSection() {
   return (
     <>
       <Row className='footer-container d-flex justify-content-center'>
-        <p className='small'>Powered by <span><a href="https://sabi-itamae.no/" target="_blank" rel="noopener noreferrer">DEVSHAFT MEDIA</a></span></p>
+        <p className='small'>Powered by <span><a href="https://www.devshaft.media/" target="_blank" rel="noopener noreferrer">DEVSHAFT MEDIA</a></span></p>
       </Row>
     </>
   );
